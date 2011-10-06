11783511b2a2c2336b144b1cdb87eaaf3f56de06
Jeff Xiong <gigix1980@gmail.com>
2011-10-06 23:28:54 +0800

    implemented read and write of tape

--- clojure/src/turing_machine/core.clj
+++ clojure/src/turing_machine/core.clj
@@ -1,24 +1,48 @@
 (ns turing_machine.core)
 
-(defstruct machine :m_configs)
+(defstruct Machine :m_configs)
 
-(defstruct tape :squares)
+(defstruct Tape :squares)
 
 (defn create_machine
 	"Build a Turing machine with given m-configs"
 	[m_configs]
-	(struct machine m_configs)
+	(struct Machine m_configs)
 )
 
 (defn create_tape
 	"Build a blank tape on which a Turing machine can operate"
 	[]
-	(repeat 65535 "")
 	; TODO: this tape is not infinite...
+	(struct Tape (repeat 65535 ""))
 )
 
-(defn write
+(defn write_square_in_seq
+	"Internal method: write to a square in a sequence of squares"
+	[squares head location content]
+	(if (= 0 location)
+		(into head (cons content (rest squares)))
+		(write_square_in_seq (rest squares) (conj head (first squares)) (- location 1) content)
+	)
+)
+
+(defn write_square
 	"Write to a square on tape"
-	[a_tape location content]
-	; TODO: implement me to pass the test
+	[tape location content]
+	(struct Tape (write_square_in_seq (:squares tape) [] location content))
+)
+
+(defn read_square_in_seq
+	"Internal method: read content of a square in a sequence of squares"
+	[squares location]
+	(if (= 0 location) 
+		(first squares)
+		(read_square_in_seq (rest squares) (- location 1))
+	)
+)
+
+(defn read_square
+	"Read content of a square on tape"
+	[tape location]
+	(read_square_in_seq (:squares tape) location)
 )
--- clojure/test/turing_machine/core_test.clj
+++ clojure/test/turing_machine/core_test.clj
@@ -5,15 +5,28 @@
 (deftest should_create_machine_with_configurations
 	(def m_config '("config-1" "0" "" "R" "config-2"))
 	(def m_configs '(m_config))
-	(def my_machine (create_machine m_configs))
+	(def machine (create_machine m_configs))
 )
 
 (deftest should_create_blank_tape
-	(def my_tape (create_tape))
+	(def tape (create_tape))
 )
 
 (deftest should_change_content_of_sqare_on_tape
-	(def my_tape (create_tape))
-	(write my_tape 0 "x")
-	(is (= "x" (first my_tape)))
+	(def tape (create_tape))
+
+	(def tape_after_first_write (write_square tape 0 "x"))
+	(is (= "x" (read_square tape_after_first_write 0)))
+	(is (= "" (read_square tape_after_first_write 1)))
+	
+	(def tape_after_second_write (write_square tape_after_first_write 1 "y"))
+	(is (= "x" (read_square tape_after_second_write 0)))
+	(is (= "y" (read_square tape_after_second_write 1)))
+	(is (= "" (read_square tape_after_second_write 2)))
+	
+	(def tape_after_third_write (write_square tape_after_second_write 2 "z"))
+	(is (= "x" (read_square tape_after_third_write 0)))
+	(is (= "y" (read_square tape_after_third_write 1)))
+	(is (= "z" (read_square tape_after_third_write 2)))
+	(is (= "" (read_square tape_after_third_write 3)))
 )
