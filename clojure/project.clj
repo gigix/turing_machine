2b11f3f7d4a4fb75b8dbb8b727eb2e6b1542816e
Jeff Xiong <gigix1980@gmail.com>
2011-10-10 22:17:06 +0800

    we don't need test suite file, either

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,5 +1,5 @@
 (defproject turing_machine "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A Turing Machine (as described in Turing's famous essay)"
   :dependencies [
 	[org.clojure/clojure "1.2.1"]
 	[org.clojure/clojure-contrib "1.2.0"]
